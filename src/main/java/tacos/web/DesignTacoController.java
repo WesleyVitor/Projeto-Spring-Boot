587a8d0bede077908487f2111f85de3b7076daf6
WesleyVitor <wesleydemorais@outlook.com.br>
2021-07-27 19:07:15 -0300

    Capitulo 2 finalizado

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,12 +3,16 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Design;
 import tacos.Ingredient;
 import tacos.Taco;
 import tacos.Ingredient.Type;
-import java.util.ArrayList;
+
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,10 +42,20 @@ public class DesignTacoController {
         }
 
 
-        model.addAttribute("design",new Taco());
-        
+        model.addAttribute("taco",new Taco());
+
 
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors){
+        if(errors.hasErrors()){
+            return "design";
+        }
+        log.info("Processing design:"+design);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(
